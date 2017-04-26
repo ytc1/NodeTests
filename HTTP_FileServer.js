4c4d61289dd2a773d1bc8c265ba8dfa4b1de013b
Yechiel Cohen <ytc92@yahoo.com>
2017-04-26 12:48:36 -0400

    Solution with Offical Solution

--- HTTP_FileServer.js
+++ HTTP_FileServer.js
@@ -6,7 +6,6 @@ var portNumber = process.argv[2];
 var dst;
 var server = http.createServer(function (req, res) {
   var src = fs.createReadStream(process.argv[3]);
-  src.pipe(dst)
-  server.res(dst);
+  src.pipe(res)
 })
 server.listen(Number(portNumber));

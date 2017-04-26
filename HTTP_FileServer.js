3e101e40eba584ad4571cccade1c1da89c1fb464
Yechiel Cohen <ytc92@yahoo.com>
2017-04-26 11:51:59 -0400

    Added HTTP_FileServer

---
+++ HTTP_FileServer.js
@@ -0,0 +1,14 @@
+
+var fs = require('fs');
+var http = require('http')
+var portNumber = process.argv[2];
+
+var dst;
+var server = http.createServer(function (req, res) {
+     var src =  fs.createReadStream(process.argv[3]);
+    src.pipe(dst)
+    server.res(dst);
+    
+
+     })
+     server.listen(Number(portNumber));

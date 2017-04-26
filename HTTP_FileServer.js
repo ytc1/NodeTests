4942e5dbffab0e9242b1e4ab6df81f9abcb6dd5b
Shaya Cohen <shayac1@gmail.com>
2017-04-26 12:00:27 -0400

    const all the things

--- HTTP_FileServer.js
+++ HTTP_FileServer.js
@@ -1,11 +1,11 @@
 
-var fs = require('fs');
-var http = require('http')
-var portNumber = process.argv[2];
+const fs = require('fs');
+const http = require('http')
+const portNumber = process.argv[2];
 
-var dst;
-var server = http.createServer(function (req, res) {
-  var src = fs.createReadStream(process.argv[3]);
+const dst;
+const server = http.createServer(function (req, res) {
+  const src = fs.createReadStream(process.argv[3]);
   src.pipe(dst)
   server.res(dst);
 })
